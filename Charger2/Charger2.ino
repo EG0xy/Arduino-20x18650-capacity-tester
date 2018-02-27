fb677ec1b8c09680cab216fc98d4784386996a0b
EGOxy <bd0c0155@opayq.com>
2018-02-27 02:38:04 +0200

    multiplexers working. added pwm. fixing auto pwm adjustment.

--- Charger2/Charger2.ino
+++ Charger2/Charger2.ino
@@ -29,6 +29,14 @@ D48 - Multiplexer HC4067 (S3 pin) Read control
 2 x PCA9685PW Adresses in l2C: 0x40 and 0x41 
 16x2 display Adress in l2C: 0x3F
 
+0-15	MUX 1 
+0-3		MUX 2 - Charged read out from Led R1 in TP4056 (NC)
+4		MUX 2 - NC (not connected)
+5-15	MUX 2
+0-8		MUX 3 - Voltage read out from batteries
+9-15	MUX 3
+0-12	MUX 4 - Voltage read out from ressistor
+
 81.85931902590525 - 0.11406781021018966x + 0.00007701200364055409x2 - 2.666253770521e-8x3 + 3.54777456e-12x4 - formula for % diff from OPUS
 */
 #define USE_SERIAL
@@ -46,9 +54,11 @@ D48 - Multiplexer HC4067 (S3 pin) Read control
 #define MUX_4_SIG A4
 #define MUX_1_EN 43
 #define MUX_2_EN 45
-#define MUX_3_EN 46
-#define MUX_4_EN 47
+#define MUX_3_EN 47
+#define MUX_4_EN 49
 
+#define PWM_DRIVER_COUNT 2
+#define PWM_CH_COUNT 16
 #define MAX_PWM 4095
 
 //Button input
@@ -56,85 +66,177 @@ D48 - Multiplexer HC4067 (S3 pin) Read control
 
 #define BAT_COUNT 20
 #define BAT_READ_COUNT 10
-const float m_cfBatteryDischargeVoltage = 4.1;
-const float m_cfBatteryCutOffVoltage = 2.85;
+const float m_cfBatteryDischargeVoltage = 4.1;//V
+const float m_cfBatteryCutOffVoltage = 2.85;//V
+const float m_cfDischargeCurrent = 1000;//mA
+static int currentMUX = 0;
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address for a 16 chars and 2 line display
 //Multiplexer control
 CD74HC4067 mux(MUX_S0, MUX_S1, MUX_S2, MUX_S3);
 
+Adafruit_PWMServoDriver pwmDriver[PWM_DRIVER_COUNT] = {Adafruit_PWMServoDriver(0x40),Adafruit_PWMServoDriver(0x41)};
+
 
 enum class EBatteryState :int
 {
-	Empty = 0,
-	PreCharge, //for TP4056 to charge up the batteries before disharging batteries need to be lower then 4V for TP4056 to start carging (Adding load until lower then 4V)
+	Empty = 0,//Voltage visible if lower then m_cfBatteryDischargeVoltage
+	PreDischarge, //for TP4056 to charge up the batteries before disharging batteries need to be lower then 4V for TP4056 to start carging (Adding load until lower then 4V)
 	Charging, //charging from TP4056 
 	Resistance,
 	Discharging,
-	Recharging,
+	Recharging, //to 3.7V
 	Finished//results
+};
+
+uint8_t muxSig[MUX_COUNT] = { MUX_1_SIG, MUX_2_SIG, MUX_3_SIG, MUX_4_SIG };
+int muxEN[MUX_COUNT] = { MUX_1_EN, MUX_2_EN, MUX_3_EN, MUX_4_EN };
+
+struct Data
+{
+	int chanel;
+	int id;
+
+	Data() :chanel(0), id(0){};
 };
 
 struct Batterie
 {
+	Data batterieReadPinsData;
+	Data ressistorReadPinsData;
+	Data pwmPinsData;
 	EBatteryState state;
 	unsigned int pwm;
 	float capacity;
 	float resistance;
 	float voltage;
 	float loadVoltage;
+	float dischargeCurrent;
 	unsigned long lasTime;
 
-	Batterie() :state(EBatteryState::Empty), pwm(0), capacity(0.f), resistance(0.f), voltage(0.f), loadVoltage(0.f), lasTime(0) {};
+	Batterie() :state(EBatteryState::Empty), pwm(0), capacity(0.f), resistance(0.f), voltage(0.f), loadVoltage(0.f), dischargeCurrent(m_cfDischargeCurrent), lasTime(0) {};
 };
 
 Batterie batteries[BAT_COUNT];
 
 
 //read voltage from analog pins
-float getVoltage(uint8_t pin, float voltageDeviderValue = 1.0) {                     
+float getVoltage(const Data& pinData, float voltageMultiplierValue = 1.0) {
 	float sample = 0.0;
-	analogRead(pin);//to reduce errors for bad reads skip first read
+	//Enabling the proper multiplexer for read sequences
+	if (currentMUX != pinData.id)
+	{
+		digitalWrite(muxEN[currentMUX], HIGH);
+		digitalWrite(muxEN[pinData.id], LOW);
+		currentMUX = pinData.id;
+	}
+	mux.channel(pinData.chanel);
+	analogRead(muxSig[pinData.id]);//to reduce errors for bad reads skip first read
 	for (uint8_t i = 0; i < BAT_READ_COUNT; i++) {
-		sample += analogRead(pin);
+		sample += analogRead(muxSig[pinData.id]);
 	}
 	sample = sample / BAT_READ_COUNT;
 
 	//voltage is read from voltage divider so multiplication by 2 is necesary
-	return (float)sample * voltageDeviderValue * (5.0 / 1023.0);
+	return (float)sample * voltageMultiplierValue * (5.0 / 1023.0);
 }
 
-//void UpdateBatteryVoltage(int batID)//from 0
-//{
-//	float fVolts = 0.f;
-//	
-//	mux.channel(batID);
-//	delay(1);
-//	fVolts = getVoltage(MUX_SIG);
-//
-//	batteries[batID].voltage = fVolts * voltageCalibration[batID];
-//}
-//void UpdateCapacity(int batID)
-//{
-//	unsigned long nDeltaTime = millis() - batteries[batID].lasTime;
-//	batteries[batID].capacity += currentCalibration[batID] * (float)nDeltaTime / 3600000.f;
-//	batteries[batID].lasTime += nDeltaTime;
-//}
+void UpdateBatteries()
+{
+	//load voltage and batterie voltage reads in separate loops in order to minimize the multiplexer switching
+	for (int i = 0; i < BAT_COUNT; ++i)
+	{
+		batteries[i].voltage = getVoltage(batteries[i].batterieReadPinsData, 2.f);
+	}
+	for (int i = 0; i < BAT_COUNT; ++i)
+	{
+		batteries[i].loadVoltage = getVoltage(batteries[i].ressistorReadPinsData);
+	}
+}
+void UpdateCapacity(float current,int batID)
+{
+	unsigned long nDeltaTime = millis() - batteries[batID].lasTime;
+	batteries[batID].capacity += current * (float)nDeltaTime / 3600000.f;
+	batteries[batID].lasTime += nDeltaTime;
+}
 
-//void Update(int batID)
-//{
-//	UpdateBatteryVoltage(batID);
-//
-//#ifdef USE_SERIAL
-//	Serial.print(batteries[batID].voltage);
-//	Serial.println();
-//#endif // USE_SERIAL
-//}
+void SetPWM(float current, int batID) {
+	bool bChanged = false;
+	if (current < batteries[batID].dischargeCurrent && batteries[batID].pwm < MAX_PWM) {//for 8bit=255, for 16bit=65535
+		batteries[batID].pwm += 1;
+		bChanged = true;
+	}
+	else if (current > batteries[batID].dischargeCurrent && batteries[batID].pwm > 0) {
+		batteries[batID].pwm -= 1;
+		bChanged = true;
+	}
+
+	if(bChanged)
+		pwmDriver[batteries[batID].pwmPinsData.id].setPWM(batteries[batID].pwmPinsData.chanel, 0, batteries[batID].pwm);
+}
+
+void Update(int batid)
+{
+	if (batteries[batid].state == EBatteryState::Discharging)
+	{
+		UpdateCapacity(batteries[batid].loadVoltage * 1000.f, batid);
+	}
+
+	SetPWM(batteries[batid].loadVoltage * 1000.f, batid);
+#ifdef USE_SERIAL
+	Serial.print(batteries[batid].loadVoltage);
+	Serial.print(" ");
+#endif // use_serial
+}
 
 void setup() {
+	Serial.begin(9600);
+	//  0 - 15	MUX 0
+	//	0 - 3	MUX 1 - Charged read out from Led R1 in TP4056(NC)
+	//	4		MUX 1 - NC(not connected)
+	//	5 - 15	MUX 1
+	//	0 - 8	MUX 2 - Voltage read out from batteries
+	//	9 - 15	MUX 2
+	//	0 - 12	MUX 3 - Voltage read out from ressistor
+
+	int muxBVNum = 1;
+	int muxCH_BVNum = 5;
+	int muxRVNum = 2;
+	int muxCH_RVNum = 9;
+	int pwmPICNum = 0;
+	int pwmCHNum = 0;
 	for (int i = 0; i < BAT_COUNT; ++i)
 	{
 		batteries[i] = Batterie();
+
+		batteries[i].pwm = 4000;
+
+		if (muxCH_BVNum >= MUX_CH_COUNT)
+		{
+			muxCH_BVNum = 0;
+			++muxBVNum;
+		}
+
+		batteries[i].batterieReadPinsData.chanel = muxCH_BVNum++;
+		batteries[i].batterieReadPinsData.id = muxBVNum;
+
+		if (muxCH_RVNum >= MUX_CH_COUNT)
+		{
+			muxCH_RVNum = 0;
+			++muxRVNum;
+		}
+
+		batteries[i].ressistorReadPinsData.chanel = muxCH_RVNum++;
+		batteries[i].ressistorReadPinsData.id = muxRVNum;
+
+		if (pwmCHNum >= PWM_CH_COUNT)
+		{
+			pwmCHNum = 0;
+			++pwmPICNum;
+		}
+
+		batteries[i].pwmPinsData.chanel = pwmCHNum++;
+		batteries[i].ressistorReadPinsData.id = pwmPICNum;
 	}
 
 	lcd.setBacklight(LOW);
@@ -160,16 +262,30 @@ void setup() {
 	digitalWrite(MUX_S2, LOW);
 	digitalWrite(MUX_S3, LOW);
 
+	digitalWrite(MUX_1_EN, LOW);
+	digitalWrite(MUX_2_EN, LOW);
+	digitalWrite(MUX_3_EN, LOW);
+	digitalWrite(MUX_4_EN, LOW);
+
 	//buttons
 	pinMode(A0, INPUT);
-	//***********************************************
-	SPI.setBitOrder(LSBFIRST);
-	// SPI_CLOCK_DIV2 is only a tiny bit faster in sending out the last byte.
-	// SPI transfer and calculations overlap for the other bytes.
-	SPI.setClockDivider(SPI_CLOCK_DIV2);
-	SPI.begin();
+
+	//PWM
+	for (size_t i = 0; i < PWM_DRIVER_COUNT; i++)
+	{
+		pwmDriver[i].begin();
+		pwmDriver[i].setPWMFreq(1600);
+	}
 }
 
 void loop() {
+	UpdateBatteries();
 
+	for (int i = 0; i < BAT_COUNT; ++i)
+	{
+		Update(i);
+	}
+#ifdef USE_SERIAL
+	Serial.println();
+#endif
 }
--- Charger2/Debug/Charger2.cpp.d
+++ Charger2/Debug/Charger2.cpp.d
@@ -13,11 +13,9 @@ C:\Users\Homer\AppData\Local\Temp\VMBuilds\Charger2\mega_atmega2560\Debug\Charge
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\ShiftPWM/ShiftPWM.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\ShiftPWM/pins_arduino_compile_time.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\ShiftPWM/CShiftPWM.h \
  C:\Users\Homer\Documents\Arduino\libraries\LiquidCrystal_I2C/LiquidCrystal_I2C.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Users\Homer\Documents\Arduino\libraries\CD74HC4067\src/CD74HC4067.h
+ C:\Users\Homer\Documents\Arduino\libraries\CD74HC4067\src/CD74HC4067.h \
+ C:\Users\Homer\Documents\Arduino\libraries/Adafruit_PWM_Servo_Driver_Library\Adafruit_PWMServoDriver.h

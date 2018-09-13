1525cf6138dfd851542ff37386758c68216a9b3c
EG0xy <bd0c0155@opayq.com>
2018-09-13 23:56:56 +0300

    Debug removed

--- V3/Debug/Wire/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Hamer\AppData\Local\Temp\VMBuilds\V3\mega_atmega2560\Debug\Wire\Wire.cpp.o: \
- C:\arduino-1.8.5\hardware\arduino\avr\libraries\Wire\src\Wire.cpp \
- C:\arduino-1.8.5\hardware\arduino\avr\libraries\Wire\src\utility/twi.h \
- C:\arduino-1.8.5\hardware\arduino\avr\libraries\Wire\src\Wire.h \
- C:\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Print.h \
- C:\arduino-1.8.5\hardware\arduino\avr\cores\arduino/WString.h \
- C:\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Printable.h
--- V3/Debug/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Hamer\AppData\Local\Temp\VMBuilds\V3\mega_atmega2560\Debug\Wire\utility\twi.c.o: \
- C:\arduino-1.8.5\hardware\arduino\avr\libraries\Wire\src\utility\twi.c \
- C:\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\arduino-1.8.5\hardware\arduino\avr\cores\arduino/binary.h \
- C:\arduino-1.8.5\hardware\arduino\avr\variants\mega/pins_arduino.h \
- C:\arduino-1.8.5\hardware\arduino\avr\libraries\Wire\src\utility\twi.h
